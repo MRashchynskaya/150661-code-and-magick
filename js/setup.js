9bcaf19a9a2784ec80751ec85b408f3c9039510d
MRashchynskaya <m.redqueen@gmail.com>
2018-09-09 22:00:22 +0300

    fixed code style

--- js/setup.js
+++ js/setup.js
@@ -1,43 +1,49 @@
 'use strict';
 
-// Показываем блок .setup, убрав у него класс .hidden
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-
-// массивы имен и фамилий
+// КОНСТАНТЫ: имена, фамилии, цвета, кол-во игроков
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-
-// массивы цветов для мантии и для глаз персонажа
 var COLORS_COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var COLORS_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var TOTAL_PLAYERS = 4;
 
+// переменные
+var userDialog = document.querySelector('.setup');
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+var players = [];
+
+// Показываем блоки .setup и .setup-similar, убирая класс .hidden
+userDialog.classList.remove('hidden');
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+// функции
 // Объявляем функцию генерации случайных данных (одну для всех случаев)
 var getRandomArrayElement = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-// Создаем массив игроков из 4 объектов со случайными параметрами
-var TOTAL_PLAYERS = 4;
-var players = [];
-var createPlayers = function (names, surnames, colorsCoat, colorsEyes, totalPlayers) {
-  for (var i = 0; i < totalPlayers; i++) {
-    players.push(
-        {
-          name: getRandomArrayElement(names) + ' ' + getRandomArrayElement(surnames),
-          coatColor: getRandomArrayElement(colorsCoat),
-          eyesColor: getRandomArrayElement(colorsEyes)
-        }
-    );
+// функция, возвращающая объект "игрок" с данными
+var createObjectPlayer = function() {
+  var objectPlayer = {
+    name: getRandomArrayElement(NAMES) + ' ' + getRandomArrayElement(SURNAMES),
+    coatColor: getRandomArrayElement(COLORS_COAT),
+    eyesColor: getRandomArrayElement(COLORS_EYES)
+  }
+  return objectPlayer;
+}
+
+// Создаем массив игроков со случайными параметрами и добавляем элементы на страницу
+var createPlayers = function () {
+  for (var i = 0; i < TOTAL_PLAYERS; i++) {
+    players.push(createObjectPlayer());
   }
   return players;
 };
 
-var similarListElement = userDialog.querySelector('.setup-similar-list');
-
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-  .content
-  .querySelector('.setup-similar-item');
+createPlayers();
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
@@ -47,12 +53,9 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-createPlayers(NAMES, SURNAMES, COLORS_COAT, COLORS_EYES, TOTAL_PLAYERS);
-
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < players.length; i++) {
   fragment.appendChild(renderWizard(players[i]));
 }
-similarListElement.appendChild(fragment);
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+similarListElement.appendChild(fragment);
